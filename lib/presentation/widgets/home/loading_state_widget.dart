dcf9950f33c91a30ca6734e95e627944ed7d3690
Rahimli <62797403+arahimli@users.noreply.github.com>
2026-02-23 01:43:21 +0400

    Update loading_state_widget.dart

--- lib/presentation/widgets/home/loading_state_widget.dart
+++ lib/presentation/widgets/home/loading_state_widget.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import '../../../core/design/design_system.dart';
 
@@ -16,6 +19,15 @@ class _LoadingStateWidgetState extends State<LoadingStateWidget>
   late AnimationController _controller;
   late Animation<double> _rotateAnimation;
 
+  BannerAd? _topBannerAd;
+  BannerAd? _bottomBannerAd;
+  bool _topBannerLoaded = false;
+  bool _bottomBannerLoaded = false;
+
+  static final String _adUnitId = Platform.isAndroid
+      ? 'ca-app-pub-3940256099942544/6300978111'
+      : 'ca-app-pub-3940256099942544/2934735716';
+
   @override
   void initState() {
     super.initState();
@@ -27,11 +39,37 @@ class _LoadingStateWidgetState extends State<LoadingStateWidget>
     _rotateAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
       CurvedAnimation(parent: _controller, curve: Curves.linear),
     );
+
+    _loadBanners();
+  }
+
+  void _loadBanners() {
+    _topBannerAd = BannerAd(
+      adUnitId: _adUnitId,
+      size: AdSize.banner,
+      request: const AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (_) => setState(() => _topBannerLoaded = true),
+        onAdFailedToLoad: (ad, _) => ad.dispose(),
+      ),
+    )..load();
+
+    _bottomBannerAd = BannerAd(
+      adUnitId: _adUnitId,
+      size: AdSize.banner,
+      request: const AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (_) => setState(() => _bottomBannerLoaded = true),
+        onAdFailedToLoad: (ad, _) => ad.dispose(),
+      ),
+    )..load();
   }
 
   @override
   void dispose() {
     _controller.dispose();
+    _topBannerAd?.dispose();
+    _bottomBannerAd?.dispose();
     super.dispose();
   }
 
@@ -39,57 +77,89 @@ class _LoadingStateWidgetState extends State<LoadingStateWidget>
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
 
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              AnimatedBuilder(
-                animation: _rotateAnimation,
-                builder: (context, child) {
-                  return Transform.rotate(
-                    angle: _rotateAnimation.value * 2 * 3.14159,
-                    child: Container(
-                      width: screenWidth * 0.3,
-                      height: screenWidth * 0.3,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(color: AppColors.accent, width: 3),
-                        gradient: const LinearGradient(
-                          colors: [AppColors.accent, AppColors.accentOverlay],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-              Container(
-                width: screenWidth * 0.2,
-                height: screenWidth * 0.2,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: AppColors.background,
-                  border: Border.all(color: AppColors.accentBorder, width: 2),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: AppSpacing.xxxl),
-          Text(widget.loadingMessage, style: AppTextStyles.loadingMessage),
-          const SizedBox(height: AppSpacing.md),
-          SizedBox(
-            width: screenWidth * 0.5,
-            child: const LinearProgressIndicator(
-              backgroundColor: Colors.white10,
-              valueColor: AlwaysStoppedAnimation<Color>(AppColors.accent),
+    return Column(
+      children: [
+        if (_topBannerLoaded && _topBannerAd != null)
+          Padding(
+            padding: const EdgeInsets.only(
+              top: AppSpacing.md,
+              left: AppSpacing.lg,
+              right: AppSpacing.lg,
+            ),
+            child: SizedBox(
+              width: _topBannerAd!.size.width.toDouble(),
+              height: _topBannerAd!.size.height.toDouble(),
+              child: AdWidget(ad: _topBannerAd!),
             ),
           ),
-        ],
-      ),
+        Expanded(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    AnimatedBuilder(
+                      animation: _rotateAnimation,
+                      builder: (context, child) {
+                        return Transform.rotate(
+                          angle: _rotateAnimation.value * 2 * 3.14159,
+                          child: Container(
+                            width: screenWidth * 0.3,
+                            height: screenWidth * 0.3,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(color: AppColors.accent, width: 3),
+                              gradient: const LinearGradient(
+                                colors: [AppColors.accent, AppColors.accentOverlay],
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    Container(
+                      width: screenWidth * 0.2,
+                      height: screenWidth * 0.2,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: AppColors.background,
+                        border: Border.all(color: AppColors.accentBorder, width: 2),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: AppSpacing.xxxl),
+                Text(widget.loadingMessage, style: AppTextStyles.loadingMessage),
+                const SizedBox(height: AppSpacing.md),
+                SizedBox(
+                  width: screenWidth * 0.5,
+                  child: const LinearProgressIndicator(
+                    backgroundColor: Colors.white10,
+                    valueColor: AlwaysStoppedAnimation<Color>(AppColors.accent),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        if (_bottomBannerLoaded && _bottomBannerAd != null)
+          Padding(
+            padding: const EdgeInsets.only(
+              bottom: AppSpacing.md,
+              left: AppSpacing.lg,
+              right: AppSpacing.lg,
+            ),
+            child: SizedBox(
+              width: _bottomBannerAd!.size.width.toDouble(),
+              height: _bottomBannerAd!.size.height.toDouble(),
+              child: AdWidget(ad: _bottomBannerAd!),
+            ),
+          ),
+      ],
     );
   }
 }
